35a7720cc80bdc6ee218fab2288052eade2a3d56
Tech-A08 <tech-a08@MacBook-Air-3.local>
2019-04-15 09:34:35 +0100

    cursor changes into crosshair on the gamescreen

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(event){
+  function mouseClick(){
+    document.getElementById("game-screen").style.cursor = "crosshair";
+  }
+
+  mouseClick()
+})
