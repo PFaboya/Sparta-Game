d3ff2f786350a99362b8adb81027fd93205fa430
Tech-A08 <tech-a08@MacBook-Air-3.local>
2019-04-16 11:38:11 +0100

    2 deers now in game

--- js/main.js
+++ js/main.js
@@ -8,76 +8,107 @@ document.addEventListener("DOMContentLoaded", function(event){
        heading[i].innerHTML = ""
       }
       document.getElementsByTagName("h3")[0].innerHTML = ""
-      function move() {
-        var obstacle = document.getElementsByClassName("bush")[0];
-        var elem = document.getElementsByClassName("deer")[0];
-        obstacle.className = "col-lg-1 col-md-1 col-sm-1 bush";
-        elem.className = "col-lg-1 col-md-1 col-sm-1 deer";
-        speed = 0.5;
-        currentPos = -500;
+      function move1() {
+        var deer1 = document.getElementById("deer1");
+        var bush1 = document.getElementById("bush1");
+        // var elem = document.getElementById("deer1");
+        bush1.className = "col-lg-1 col-md-1 col-sm-1 bush";
+        deer1.className = "col-lg-1 col-md-1 col-sm-1 deer";
+        var speed = 0.5;
+        var currentPos = -440;
         // Reset the element
-        elem.style.bottom = 0+"px";
-        elem.style.top = "auto";
-        var motionInterval = setInterval(function() {
+        deer1.style.bottom = 0+"px";
+        deer1.style.top = "auto";
+        var motionInterval1 = setInterval(function() {
           currentPos += speed;
-          if (currentPos >= -350 && speed >= 0) {
-           currentPos = -350;
+          if (currentPos >= -340 && speed >= 0) {
+           currentPos = -340;
            speed = -1 * speed;
-           // elem.style.width = parseInt(elem.style.width)*2+"px";
-           // elem.style.height = parseInt(elem.style.height)*2+"px";
          }
-        if (currentPos <= -500 && speed < 0) {
-          currentPos = -500;
+        if (currentPos <= -440 && speed < 0) {
+          currentPos = -440;
           speed = -1 * speed;
          }
-         // clearInterval(motionInterval);
 
-        elem.style.bottom = currentPos+"px";
+        deer1.style.bottom = currentPos+"px";
       });
       }
-      move()
+      function move2() {
+        var deer2 = document.getElementById("deer2");
+        var bush2 = document.getElementById("bush2");
+
+        bush2.className = "col-lg-1 col-md-1 col-sm-1 bush";
+        deer2.className = "col-lg-1 col-md-1 col-sm-1 deer";
+        var speed = 0.5;
+        var currentPos = -480;
+        // Reset the element
+        deer2.style.bottom = 0+"px";
+        deer2.style.top = "auto";
+        var motionInterval2 = setInterval(function() {
+          currentPos += speed;
+          if (currentPos >= -450 && speed >= 0) {
+           currentPos = -450;
+           speed = -1 * speed;
+         }
+        if (currentPos <= -550 && speed < 0) {
+          currentPos = -550;
+          speed = -1 * speed;
+         }
+
+        deer2.style.bottom = currentPos+"px";
+      });
+      }
+      move1()
+      move2()
     })
   }
 
   function mouseClick(){
+    var deer = document.getElementsByClassName("deer");
     points = 0
-    // document.getElementById("points").innerHTML = points
     document.getElementById("game-screen").style.cursor = "crosshair";
-    var hit = document.getElementsByClassName("deer")[0]
-    hit.addEventListener("click", function(){
-      console.log("it works")
-      points += 10
-      document.getElementById("points").innerHTML = points
-    })
+    for (var i = 0; i < deer.length; i++) {
+      var deers = deer[i]
+      deers.addEventListener("click", function(event){
+        console.log("it works");
+        points += 10;
+        document.getElementById("points").innerHTML = points;
+        event.target.style.visibility = "hidden";
+        var reappear = setInterval(function(){
+          event.target.style.visibility = "visible";
+        }, 1000);
+      })
+    }
   }
 
-  var button = document.getElementById("start")
-  button.addEventListener("click", function(){
-    var timeleft = 30;
-    var timer = setInterval(function(){
-      if (timeleft >= 1){
-        document.getElementById("time").innerHTML = timeleft - 1;
-      }
-      timeleft -= 1;
-      if(timeleft == 0){
-        console.log("3 sec up");
-        var endGame = document.getElementById("game-screen");
-        endGame.innerHTML = "Time is Up Your Score is " + points;
-        endGame.style.color = "red";
-        endGame.style.fontSize = "90px";
-        var res = document.getElementsByClassName("again")[0];
-        res.className = "col-lg-6 col-md-6 text-center again";
-        document.getElementsByClassName("menu")[0].className = "hidden-lg hidden-md hidden-sm"
-        document.getElementById("reset").addEventListener("click", function(){
-          location.reload()
-        })
-      };
-    }, 1000)
-  // countDown()
-  })
-
+  function countDown(){
+    var button = document.getElementById("start")
+    button.addEventListener("click", function(){
+      var timeleft = 30;
+      var timer = setInterval(function(){
+        if (timeleft >= 1){
+          document.getElementById("time").innerHTML = timeleft - 1;
+        }
+        timeleft -= 1;
+        if(timeleft == 0){
+          console.log("3 sec up");
+          var endGame = document.getElementById("game-screen");
+          endGame.innerHTML = "Time is Up Your Score is " + points;
+          endGame.style.color = "red";
+          endGame.style.fontSize = "90px";
+          var res = document.getElementsByClassName("again")[0];
+          res.className = "col-lg-6 col-md-6 text-center again";
+          document.getElementsByClassName("menu")[0].className = "hidden-lg hidden-md hidden-sm"
+          document.getElementById("reset").addEventListener("click", function(){
+            location.reload()
+          })
+        };
+      }, 1000)
+    })
+  }
 
 
   startGame()
   mouseClick()
+  countDown()
 })
