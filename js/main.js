fba240936065bdb240742d9a05c305d4c609403f
Tech-A08 <tech-a08@MacBook-Air-3.local>
2019-04-16 14:50:39 +0100

    4 deers

--- js/main.js
+++ js/main.js
@@ -14,21 +14,33 @@ document.addEventListener("DOMContentLoaded", function(event){
         // var elem = document.getElementById("deer1");
         bush1.className = "col-lg-1 col-md-1 col-sm-1 bush";
         deer1.className = "col-lg-1 col-md-1 col-sm-1 deer";
-        var speed = 0.5;
+        var speed = 1;
         var currentPos = -440;
         // Reset the element
         deer1.style.bottom = 0+"px";
         deer1.style.top = "auto";
+        var timeleft = 3;
         var motionInterval1 = setInterval(function() {
           currentPos += speed;
-          if (currentPos >= -340 && speed >= 0) {
-           currentPos = -340;
-           speed = -1 * speed;
-         }
-        if (currentPos <= -440 && speed < 0) {
-          currentPos = -440;
-          speed = -1 * speed;
-         }
+          if (currentPos >= -340 && speed > 0) {
+            currentPos = -340;
+            speed = 0;
+          }
+          else if(currentPos == -340 && speed == 0 && timeleft >= 0){
+            currentPos = -340;
+            setInterval(function() {
+              timeleft -= 1;
+            }, 1000)
+            console.log(timeleft)
+          }
+          else if(timeleft == 0){
+            currentPos = -340;
+            speed = -1;
+          }
+          else if (currentPos <= -440 && speed < 0) {
+            currentPos = -440;
+            speed = -1 * speed;
+          }
 
         deer1.style.bottom = currentPos+"px";
       });
@@ -58,8 +70,60 @@ document.addEventListener("DOMContentLoaded", function(event){
         deer2.style.bottom = currentPos+"px";
       });
       }
+      function move3() {
+        var deer3 = document.getElementById("deer3");
+        var bush3 = document.getElementById("bush3");
+
+        bush3.className = "col-lg-1 col-md-1 col-sm-1 bush";
+        deer3.className = "col-lg-1 col-md-1 col-sm-1 deer";
+        var speed = 0.5;
+        var currentPos = -440;
+        // Reset the element
+        deer3.style.bottom = 0+"px";
+        deer3.style.top = "auto";
+        var motionInterval3 = setInterval(function() {
+          currentPos += speed;
+          if (currentPos >= -380 && speed >= 0) {
+           currentPos = -380;
+           speed = -1 * speed;
+         }
+        if (currentPos <= -480 && speed < 0) {
+          currentPos = -480;
+          speed = -1 * speed;
+         }
+
+        deer3.style.bottom = currentPos+"px";
+      });
+      }
+      function move4() {
+        var deer4 = document.getElementById("deer4");
+        var bush4 = document.getElementById("bush4");
+
+        bush4.className = "col-lg-1 col-md-1 col-sm-1 bush";
+        deer4.className = "col-lg-1 col-md-1 col-sm-1 deer";
+        var speed = 0.5;
+        var currentPos = -270;
+        // Reset the element
+        deer4.style.bottom = 0+"px";
+        deer4.style.top = "auto";
+        var motionInterval4 = setInterval(function() {
+          currentPos += speed;
+          if (currentPos >= -200 && speed >= 0) {
+           currentPos = -200;
+           speed = -1 * speed;
+         }
+        if (currentPos <= -300 && speed < 0) {
+          currentPos = -300;
+          speed = -1 * speed;
+         }
+
+        deer4.style.bottom = currentPos+"px";
+      });
+      }
       move1()
       move2()
+      move3()
+      move4()
     })
   }
 
@@ -76,7 +140,7 @@ document.addEventListener("DOMContentLoaded", function(event){
         event.target.style.visibility = "hidden";
         var reappear = setInterval(function(){
           event.target.style.visibility = "visible";
-        }, 1000);
+        }, (Math.random() * 10000) + 3000);
       })
     }
   }
